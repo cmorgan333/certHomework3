f4a313f6f8fa76c603e8d2212b224e2d30d978e6
cmorgan333 <78804785+cmorgan333@users.noreply.github.com>
2021-10-28 15:26:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# certHomework3
+ 
---
+++ app/app.js
@@ -0,0 +1,31 @@
+function changeRoute() {
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace("#", "");
+
+    MODEL.updateView(pageID)
+}
+
+function initURLListener(){
+    $(window).on("hashchange", changeRoute);
+    changeRoute();
+}
+
+
+function initListeners() {
+    $(".bars").click(function (e) {
+        $(".bars").toggleClass("active");
+        $(".links").toggleClass("active");
+    });
+
+    $(".links a").click(function (e) {
+        $(".bars").toggleClass("active");
+        $(".links").toggleClass("active");
+    });
+}
+
+
+
+$(document).ready(function() {
+    initURLListener();
+    initListeners();
+});
